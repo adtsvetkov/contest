3577ea8fa0f1a0c707bfac0468524de6b91e20ab
adtsvetkov <41506048+adtsvetkov@users.noreply.github.com>
2021-01-10 19:45:54 +0300

    Update task.py

--- tasks/task1/task.py
+++ tasks/task1/task.py
@@ -1,1 +1,7 @@
-
+def task1(transitions: list, mouses: list) -> int:
+  """
+    :type transitions (list):  transitions between burrows
+    :type mouses (list): number of mouses in each burrow at the beginning
+    :return: number of catched mouses
+  """
+  pass
