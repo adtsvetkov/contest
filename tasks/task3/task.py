09b9836f8636629308c1708777e06e07382f36d0
adtsvetkov <41506048+adtsvetkov@users.noreply.github.com>
2021-01-10 22:44:01 +0300

    Update task.py

--- tasks/task3/task.py
+++ tasks/task3/task.py
@@ -1,5 +1,7 @@
-def task3(pixels: list) -> int:
+def task3(n: int, m: int, pixels: list) -> int:
   """
+    :type n (int): number of rows of screen
+    :type m (int): number of columns of screen
     :type pixels (list): list of pixels which are working
     :return: minimum cost of travelling
   """
