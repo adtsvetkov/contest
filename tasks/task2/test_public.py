8606c87b3183a5c5cbed01d4d8e493525172678f
adtsvetkov <41506048+adtsvetkov@users.noreply.github.com>
2021-01-10 22:10:16 +0300

    Update test_public.py

--- tasks/task2/test_public.py
+++ tasks/task2/test_public.py
@@ -1,1 +1,37 @@
+import pytest
+
+from .task import task2
+
+
+class Case:
+    def __init__(self, name: str, coords: list, city: list, optimal_cost: float):
+        self._name = name
+        self.coords = coords
+        self.city = city
+        self.optimal_cost = optimal_cost
+
+    def __str__(self) -> str:
+        return 'task2_test_{}'.format(self._name)
+
+
+TEST_CASES = [
+    Case(
+        name='base1',
+        coords=[(0, 3), (10, 3)],
+        city=[(1, 0), (9, 0), (5, 3)],
+        optimal_cost = 10.000000,
+    ),
+    Case(
+        name='base2',
+        transitions=[(-90, 50), (90, 50)],
+        mouses=[(0, 0), (1, 100), (-1, 100)],
+        optimal_cost = 181.000000,
+    ),
+]
+
+
+@pytest.mark.parametrize('case', TEST_CASES, ids=str)
+def test_task2(case: Case) -> None:
+    cost = task2(coords=case.coords, city = case.city)
+    assert cost == case.optimal_cost
 
