5c945c3ef4bdadc53f73a6a35ffddfa9c9e56291
adtsvetkov <41506048+adtsvetkov@users.noreply.github.com>
2021-01-10 22:01:24 +0300

    Update task.py

--- tasks/task2/task.py
+++ tasks/task2/task.py
@@ -1,1 +1,7 @@
-
+def task2(coords: list, city: list) -> float:
+  """
+    :type coords (list): list of two tuples of warehouse and house coordinats
+    :type city (list): list of apexes of the city counterclock-wise
+    :return: minimum cost of delivery
+  """
+  pass
